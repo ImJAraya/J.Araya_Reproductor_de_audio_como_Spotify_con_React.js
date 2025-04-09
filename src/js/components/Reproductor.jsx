68dfa936df4458425c8607159a00650a3f655701
ImJAraya <fabiarayamontero@gmail.com>
2025-04-08 22:19:03 -0600

    Reparado los botones

--- src/js/components/Reproductor.jsx
+++ src/js/components/Reproductor.jsx
@@ -85,15 +85,15 @@ const Reproductor = () => {
         <div >
             {songs.map((song, index) => (
                 <div key={index} className="card col" onClick={() => handlerCancion(index)}>
-                    {`${index + 1} ${song.name}`}
+                    {`${index + 1} ${song.name} ${indice === index ? " (Playing)" : ""}` }
                 </div>
             ))}
             <audio ref={audioRef} src={url}></audio>
             <div className="controlButtons gap-3">
-                <Boton icono={<FontAwesomeIcon icon={faBackward} onClick={handlerBackward} />} />
-                <Boton icono={<FontAwesomeIcon icon={faPause} onClick={handlerPause} />} />
+                <Boton icono={<FontAwesomeIcon icon={faBackward}/>} onClick={handlerBackward}/>
+                <Boton icono={<FontAwesomeIcon icon={faPause} />} onClick={handlerPause} />
                 <Boton icono={<FontAwesomeIcon icon={faPlay} />} onClick={handlerPlay} />
-                <Boton icono={<FontAwesomeIcon icon={faForward} onClick={handlerForward} />} />
+                <Boton icono={<FontAwesomeIcon icon={faForward} />} onClick={handlerForward}/>
             </div>
         </div>
     )
